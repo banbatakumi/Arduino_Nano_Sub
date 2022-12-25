1948e76ca7a4f0307c55dfd31998a93d3bad42a9
banbatakumi <bantech0210@gmail.com>
2022-12-25 17:32:13 +0900

    超音波追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,31 @@
 #include <Arduino.h>
 #include <Pixy2I2C.h>
+
 Pixy2I2C pixy;
+
+#define TRIG_ECHO_PIN 14
+#define DISTABCE_RC 0.6
+#define LED_PIN 13
+
+uint16_t pre_distance, distance, duration;
 
 void setup() {
       Serial.begin(19200);
-      while (!Serial)
-            ;
       pixy.init();
+
+      pinMode(LED_PIN, OUTPUT);
+      for (uint8_t count = 0; count <= 10; count++) {
+            digitalWrite(LED_PIN, HIGH);
+            delay(100);
+            digitalWrite(LED_PIN, LOW);
+            delay(100);
+      }
 }
 
+void (*resetFunc)(void) = 0;
+
 void loop() {
-      short yellow_angle = 0, blue_angle = 0, yellow_wide = 0, blue_wide = 0, old_yellow_wide = 0, old_blue_wide = 0;
+      int16_t yellow_angle = 0, blue_angle = 0, yellow_wide = 0, blue_wide = 0, old_yellow_wide = 0, old_blue_wide = 0;
 
       pixy.ccc.getBlocks();
       if (pixy.ccc.numBlocks) {
@@ -27,8 +42,29 @@ void loop() {
                   }
             }
       }
+
+      pinMode(TRIG_ECHO_PIN, OUTPUT);
+      digitalWrite(TRIG_ECHO_PIN, LOW);
+      delayMicroseconds(2);
+      digitalWrite(TRIG_ECHO_PIN, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(TRIG_ECHO_PIN, LOW);
+
+      pinMode(TRIG_ECHO_PIN, INPUT);
+      duration = pulseIn(TRIG_ECHO_PIN, HIGH);   // 往復にかかった時間が返却される[マイクロ秒]
+      if (duration > 0) {
+            duration /= 2;   // 往路にかかった時間
+            distance = duration * 340.000 * 100.000 / 1000000.000;
+            distance = distance * (1 - DISTABCE_RC) + pre_distance * DISTABCE_RC;
+            pre_distance = distance;
+            if (distance > 255) distance = 255;
+      }
+
       Serial.write('a');
       Serial.write(yellow_angle);
       Serial.write(blue_angle);
+      Serial.write(distance);
       Serial.flush();
+
+      if (Serial.read() == 'a') resetFunc();   // call reset
 }
